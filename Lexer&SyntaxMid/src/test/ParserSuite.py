cd89f71d239458b5e2c27a9e63ddfab421c904dd
Ly Kien Minh <lykienminh9@gmail.com>
2022-06-26 22:11:39 +0700

    :fire: Done Lexer & Syntax Midterm

--- Lexer&SyntaxMid/src/test/ParserSuite.py
+++ Lexer&SyntaxMid/src/test/ParserSuite.py
@@ -4,12 +4,12 @@ from TestUtils import TestParser
 class ParserSuite(unittest.TestCase):
     def test_simple_program(self):
         """Simple program: int main() {} """
-        input = """Var: x;"""
+        input = """abc = 1 + 2 ?? 3;"""
         expect = "successful"
         self.assertTrue(TestParser.checkParser(input,expect,201))
     
     def test_wrong_miss_close(self):
         """Miss variable"""
-        input = """Var: ;"""
-        expect = "Error on line 1 col 5: ;"
+        input = """u = array(a1 => 3 . 4, a2 => 3 + (u2 % 5));"""
+        expect = "successful"
         self.assertTrue(TestParser.checkParser(input,expect,202))
